e2d371d3a35d66c67117c1a0fdbd87215d087b48
sarah-lima <sarah.lima@bandtec.com.br>
2021-07-19 12:11:26 -0300

    math random data

--- src/pages/Home/Index.jsx
+++ src/pages/Home/Index.jsx
@@ -5,14 +5,25 @@ import Menu from '../../components/Menu/Index'
 
 export default function Home() {
     const [chartData, setChartData] = useState({})
-
+    var valor =[]
+    const random = () =>{
+        if(valor.length>1){
+            for (var e=0; e<7;e++){
+                valor.pop()
+            }
+        }
+        for (var i=0; i<7;i++){
+            valor.push(Math.floor(Math.random()*10))
+        }
+        console.log(valor)
+    }
     const chart = () => {
         setChartData({
             labels: ['segunda', 'terça', 'quarta', 'quinta', 'sexta', 'sabado', 'domingo'],
             datasets: [
                 {
                     label: 'Texto',
-                    data: [32, 42, 23, 56, 78, 20, 60],
+                    data: valor,
                     fill: true,
                     lineTension: 0.2,
                     backgroundColor: ['#17BEBB', '#2E282A', '#CD5334', '#EDB88B', '#FAD8D6','#FAD8A5', '#2E102F'],
@@ -26,8 +37,13 @@ export default function Home() {
     }
 
     useEffect(() => {
-        chart();
+        setInterval(()=>{
+            random();
+            chart();
+        }, 10000)
     }, [])
+
+    
     return (
         <>
             <Menu />
